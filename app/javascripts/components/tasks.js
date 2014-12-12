681616086364c0dd3030fafe5b8f51232b5dfc3d
Rory Powis <rory@hikkendry.com>
2014-12-12 15:19:35 +0000

    Only show tasks a user is authed to see. Including adding and removing tasks

--- app/javascripts/components/tasks.js
+++ app/javascripts/components/tasks.js
@@ -14,27 +14,48 @@ var Tasks = React.createClass({
 
   getInitialState: function () {
     return {
-      tasks: []
+      tasks: {}
     };
   },
 
   componentWillMount: function () {
-    this.bindAsArray(new Firebase("https://jkilla.firebaseio.com/tasks/"), "tasks");
+    var tasks = {},
+        dbRef = new Firebase("https://jkilla.firebaseio.com"),
+        tasksRef = dbRef.child("tasks"),
+        usersTasks = dbRef.child("usersTasks"),
+        uid = usersTasks.getAuth().uid;
+
+    usersTasks.child(uid).on('child_added', function(childSnapshot) {
+      tasksRef.child(childSnapshot.key()).on('value', function(dataSnapshot) {
+        tasks[childSnapshot.key()] = dataSnapshot.val();
+        this.setState({
+          tasks: tasks
+        });
+      }.bind(this));
+    }.bind(this));
+
+    usersTasks.child(uid).on('child_removed', function(childSnapshot) {
+      delete this.state.tasks[childSnapshot.key()];
+      this.setState({
+        tasks: this.state.tasks
+      });
+    }.bind(this));
   },
 
   render: function() {
     var createItem = function(item, index) {
+      item = this.state.tasks[item];
       return (
         <a className="task" key={ index }>
           <h3>{ item.title }</h3>
           <span>{ item.description }</span>
         </a>
       );
-    };
+    }.bind(this);
 
     return (
       <div className="task-list">
-        { this.state.tasks.map(createItem) }
+        { Object.keys(this.state.tasks).map(createItem) }
       </div>
     );
   }
