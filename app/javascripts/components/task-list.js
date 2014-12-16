b4d9a325e2ee040347a2bba77354a4f25c7afbad
Tom van Neerijnen <tom@tomvn.com>
2014-12-16 12:50:56 +0000

    list children

--- app/javascripts/components/task-list.js
+++ app/javascripts/components/task-list.js
@@ -1,19 +1,17 @@
-var React = require('../vendor/react/react');
+var React = require('react');
 
 var TaskList = React.createClass({
   render: function() {
-    var createItem = function(item, index) {
+    var taskNodes = this.props.tasks.map(function(task) {
       return (
-        <a className="task" key={ index }>
-          <h3>{ item.title }</h3>
-          <span>{ item.description }</span>
-        </a>
+        <h3 className="taskList">
+          {task.title}
+        </h3>
       );
-    };
-
+    });
     return (
-      <div className="task-list">
-        { this.props.tasks.map(createItem) }
+      <div className="taskList">
+        {taskNodes}
       </div>
     );
   }
--- app/javascripts/components/task.js
+++ app/javascripts/components/task.js
@@ -4,7 +4,8 @@ var React = require('react'),
     Router = require('react-router'),
     { Route, RouteHandler, Link } = Router,
     auth = require('./auth').auth,
-    Authentication = require('./auth').Authentication;
+    Authentication = require('./auth').Authentication,
+    TaskList = require('./task-list');
 
 var Task = React.createClass({
   mixins: [
@@ -14,30 +15,43 @@ var Task = React.createClass({
 
   getInitialState: function () {
     return {
-      task: {},
+      task: {children:[]},
       fbRef: this.props.fbRef
     };
   },
 
   componentWillMount: function () {
-    var {taskId} = this.getParams(),
+    var taskId = this.getParams().taskId,
         dbRef = this.state.fbRef,
         tasksRef = dbRef.child("tasks");
 
     tasksRef.child(taskId).on('value', function(taskSnapshot) {
+      var task = taskSnapshot.val();
+      task.id = taskSnapshot.key();
+      task.children = [];
       this.setState({
-        task: taskSnapshot.val()
+        task: task
       });
-    }.bind(this));
+      dbRef.child('tasksEdges/'+taskId+'/child/').on('child_added', function(taskEdgeSnapshot) {
+        dbRef.child('tasks/'+taskEdgeSnapshot.key()).on('value', function(taskSnapshot) {
+          var childTask = taskSnapshot.val();
+          childTask.id = taskSnapshot.key();
+          task.children.push(childTask);
+          this.setState({
+            task: task
+          });
+        }, this);
+      }, this);
+    }, this);
   },
 
   render: function () {
     var task = this.state.task;
-    console.log('task ' , task);
     return (
       <div className="task">
         <h3>{ task.title }</h3>
         <span>{ task.description }</span>
+        <TaskList tasks={task.children} />
       </div>
     );
   }
