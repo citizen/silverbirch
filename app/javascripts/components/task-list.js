7c28181b2c71e0efd43a0f85f36532874f1ad3c0
Tom van Neerijnen <tom@tomvn.com>
2014-12-20 11:34:52 +0000

    working child links

--- app/javascripts/components/task-list.js
+++ app/javascripts/components/task-list.js
@@ -1,17 +1,21 @@
-var React = require('react');
+var React = require('react'),
+    Router = require('react-router'),
+    { Route, RouteHandler, Link } = Router;
 
 var TaskList = React.createClass({
   render: function() {
     var taskNodes = this.props.tasks.map(function(task) {
       return (
-        <h3 className="taskList">
-          {task.title}
-        </h3>
+        <li>
+          <Link to="task" params={{taskId: task.id}} key={task.key}>{task.title}</Link>
+        </li>
       );
     });
     return (
       <div className="taskList">
-        {taskNodes}
+        <ul>
+          {taskNodes}
+        </ul>
       </div>
     );
   }
--- app/javascripts/components/task.js
+++ app/javascripts/components/task.js
@@ -10,6 +10,7 @@ var React = require('react'),
 var Task = React.createClass({
   mixins: [
     Router.State,
+    Router.Navigation,
     Authentication
   ],
 
@@ -20,7 +21,15 @@ var Task = React.createClass({
     };
   },
 
-  componentWillMount: function () {
+  componentWillReceiveProps: function() {
+    this.loadTask();
+  },
+
+  componentDidMount: function () {
+    this.loadTask();
+  },
+
+  loadTask: function() {
     var taskId = this.getParams().taskId,
         dbRef = this.state.fbRef,
         tasksRef = dbRef.child("tasks");
@@ -34,7 +43,8 @@ var Task = React.createClass({
       });
       dbRef.child('tasksEdges/'+taskId+'/child/').on('child_added', function(taskEdgeSnapshot) {
         dbRef.child('tasks/'+taskEdgeSnapshot.key()).on('value', function(taskSnapshot) {
-          var childTask = taskSnapshot.val();
+          var childTask = {};
+          childTask.title = taskSnapshot.child('title').val();
           childTask.id = taskSnapshot.key();
           task.children.push(childTask);
           this.setState({
@@ -50,6 +60,7 @@ var Task = React.createClass({
     return (
       <div className="task">
         <h3>{ task.title }</h3>
+        <p>wtf -> {this.getParams().taskId}</p>
         <span>{ task.description }</span>
         <TaskList tasks={task.children} />
       </div>
