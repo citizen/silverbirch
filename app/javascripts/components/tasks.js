eadde14665f482464c48f94f68320dd2c85d267a
Jeremy Suarez <lokaleyproject@gmail.com>
2015-03-03 18:47:40 +0000

    Update task-tree markup to adopt new designs

--- app/javascripts/components/tasks.js
+++ app/javascripts/components/tasks.js
@@ -106,45 +106,10 @@ var Tasks = React.createClass({
 
   render: function() {
     return (
-
-      <div>
-      <br></br>
-
-      <h2 className="page-header">Tasks</h2>
-        <section className="comment-list">
-          <article className="row">
-            <div className="col-md-2 col-sm-2 hidden-xs">
-              <figure className="thumbnail">
-          
-                <img className="img-responsive" src="http://www.keita-gaming.com/assets/profile/default-avatar-c5d8ec086224cb6fc4e395f4ba3018c2.jpg" />
-                <figcaption className="text-center">username</figcaption>
-              </figure>
-            </div>
-            <div className="col-md-10 col-sm-10">
-              <div className="panel panel-default arrow left">
-                <div className="panel-body">
-                  <header className="text-left">
-                    <div className="comment-user"><i className="fa fa-user"></i> That Guy</div>
-                    <time className="comment-date" datetime="16-12-2014 01:05"><i className="fa fa-clock-o"></i> Dec 16, 2014</time>
-                  </header>
-                  <div className="comment-post">
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    </p>
-                  </div>
-                  <p className="text-right"><a href="#" className="btn btn-default btn-sm"><i className="fa fa-reply"></i> reply</a></p>
-                </div>
-              </div>
-            </div>
-          </article>
-          </section>
-
-<div className="row">
-  <div className="col-md-6"><TaskTree tasks={this.state.taskTree} {...this.props} /></div>
-  <div className="col-md-6"><Link to="newTask" className="btn btn-primary glyphicon glyphicon-plus">
-  </Link></div><RouteHandler {...this.props} task={this.state.currentTask} />
-</div>
-  
+      <div className="row">
+        <div className="col-md-6"><TaskTree tasks={this.state.taskTree} {...this.props} /></div>
+        <div className="col-md-6"><Link to="newTask" className="btn btn-primary glyphicon glyphicon-plus">
+        </Link></div><RouteHandler {...this.props} task={this.state.currentTask} />
       </div>
     );
   }
