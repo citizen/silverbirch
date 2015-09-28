5d903f87b6c0645d6088ef353960c2d14d91adaf
Rory Powis <rory@hikkendry.com>
2015-09-28 20:00:32 +0100

    Move nav into dropdown triggered by clicking avatar

--- app/javascripts/components/includes/header.js
+++ app/javascripts/components/includes/header.js
@@ -10,12 +10,24 @@ var Header = React.createClass({
     Router.State
   ],
 
+  getInitialState: function () {
+    return {
+      isVisible: false
+    };
+  },
+
   getViewContext: function () {
     if (this.props.user && this.props.user.properties.is_viewing) {
       return this.props.graph[this.props.user.properties.is_viewing];
     }
 
     return null;
+  },
+
+  toggleDropdown: function (e) {
+    this.setState({
+      isVisible: !this.state.isVisible
+    });
   },
 
   render: function () {
@@ -26,6 +38,7 @@ var Header = React.createClass({
     var userName,
       	teamsList,
         viewContext,
+        menuDropdown,
       	teamsDropdown,
       	viewContextName;
 
@@ -37,22 +50,19 @@ var Header = React.createClass({
 			? viewContext.properties.sbid.split(":")[1]
 			: "";
 
+    menuDropdown = (this.state.isVisible) ?
+      <div className="navigation-list">
+        <Link to="tasks" params={{viewContext: viewContextName}}>Tasks</Link>
+        <Link to="report" params={{viewContext: viewContextName}}>Report</Link>
+        <Link to="newTeam">New Team</Link>
+        <Link to="profile" params={{viewContext: this.props.user.properties.username}}>Profile</Link>
+        <Link to="logout">Sign out</Link>
+      </div> : null;
+
     return (
       <div className="navigation">
-        <div className="navigation-list">
-          <h5>
-            <Link to="profile" params={{viewContext: this.props.user.properties.username}}>
-              <img className="header-avatar" src={this.props.user.properties.avatar} />
-              <span>{this.props.user.properties.username}</span>
-            </Link>
-          </h5>
-          <Link to="tasks" params={{viewContext: viewContextName}}>Tasks</Link> |
-          <Link to="report" params={{viewContext: viewContextName}}>Report</Link>
-          <h5>
-            <Link to="logout">Sign out</Link>
-          </h5>
-          <Link to="newTeam"><span>New Team</span></Link>
-        </div>
+        <img className="header-avatar" onClick={this.toggleDropdown} src={this.props.user.properties.avatar} />
+        {menuDropdown}
 
         <div className="header-left">
           <ul>
